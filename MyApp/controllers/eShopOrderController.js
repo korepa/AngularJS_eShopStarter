cb95e33d9e159a113ff11eb865b5a71534998032
Knyazev Ivan <to.knyazev@gmail.com>
2018-02-10 16:55:36 +0300

    order

--- MyApp/controllers/eShopOrderController.js
+++ MyApp/controllers/eShopOrderController.js
@@ -1,6 +1,6 @@
 ﻿app.controller('eShopOrderController', function ($scope, $window) {
 
-    $scope.orderItemsTitles = ['Артикул', 'Товар', 'Цена', 'Количество', 'Общая цена'];
+    $scope.orderItemsTitles = ['Артикул', 'Товар', 'Цена', 'Количество', 'Общая цена', '+', '-'];
     $scope.orderItems = JSON.parse($window.localStorage.getItem('order'));
 
     $scope.summ = function () {
@@ -9,41 +9,38 @@
             result += item.price * item.count;
         }
         return result;
-    }
+    };
 
     $scope.emptyTrash = function () {
         $window.localStorage.removeItem('order');
         $scope.orderItems = null;
-    }
+    };
 
-    /*
-    console.log("id : " + itemIndex);
-    let orderItem = JSON.parse($window.localStorage.getItem('order'));
-    let newItem = { id: itemIndex, count: 1 };
-    let existed = false;
-    // инициализация
-    if (orderItem == null) {
-        orderItem = [];
-    }
-    // обновим значение
-    for (var item of orderItem) {
-        if (item.id === itemIndex) {
-            item.count += 1;
-            existed = true;
-            break;
+    // добавление товара в корзину
+    $scope.updateTrash = function (updateFlag, itemIndex, itemName, itemPrice) {
+        console.log("updateFlag : " + updateFlag);
+        console.log("id : " + itemIndex);
+        return;
+
+        let orderItem = JSON.parse($window.localStorage.getItem('order'));
+        let newItem = { id: itemIndex, name: itemName, price: itemPrice, count: 1 };
+        let existed = false;
+        // инициализация
+        if (orderItem == null) {
+            orderItem = [];
         }
+        // обновим значение
+        for (var item of orderItem) {
+            if (item.id === itemIndex) {
+                item.count += 1;
+                existed = true;
+                break;
+            }
+        }
+        // добавим значение
+        if (existed == false) {
+            orderItem.push(newItem);
+        }
+        $window.localStorage.setItem('order', JSON.stringify(orderItem));
     }
-    // добавим значение
-    if (existed == false) {
-        orderItem.push(newItem);
-    }
-    $window.localStorage.setItem('order', JSON.stringify(orderItem));
-    */
-
-
-    /*
-    $scope.getOrderFromTrash = function () {
-
-    }
-    */
 });
